df6b017cfa79f6387f28e4ad2e2d900b1c4b822c
Jhones <jhonesaly@gmail.com>
2024-01-19 23:59:55 -0300

    feat: chatgpt avatar

---
+++ web/src/assets/avatar.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Avatar = (props) =>{
+    return(
+        <svg>
+            <img draggable="false" alt="" src="chrome://favicon2/?size=24&amp;scaleFactor=1x&amp;showFallbackMonogram=&amp;pageUrl=https%3A%2F%2Fchat.openai.com%2Fchat"></img>
+        </svg>
+    )
+}
+
+export {Avatar}
