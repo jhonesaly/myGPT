29cd8daf3c44c8c7a400c0f2a251f1a7db4c92e0
Jhones <jhonesaly@gmail.com>
2024-01-23 21:01:37 -0300

    feat: hook usestate

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+
 import './styles/App.css';
 import './styles/reset.css';
 
 import {SideMenu} from './components/SideMenu';
 
 function App() {
+
+  const [input, setInput] = useState("")
+  const [chatlog, setChatLog] = useState([{
+    user: "gpt",
+    message: "Como posso te ajudar hoje?"
+  }])
+
   return (
     <div className="App">
       <SideMenu />
