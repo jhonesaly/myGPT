044bf8698a15eb6239db6e5313a1a0fb98d9e139
Jhones <jhonesaly@gmail.com>
2024-01-23 22:04:37 -0300

    fix: export bug

--- web/src/api/api.js
+++ web/src/api/api.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const URL_API = 'http://localhost:5555/api/prompt'
 
-const makeRequest = async (message)=>{
+export const makeRequest = async (message)=>{
     const{data} = await axios.post(URL_API, message)
     return data
 }
-
-export { makeRequest }
